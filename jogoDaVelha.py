1c05c66f9bab3f443822fd260aa8e817fcef5591
takara kazue <eugenia.stefens.237219@a.fecaf.com.br>
2026-03-22 15:41:27 +0900

    atualizando

--- jogoDaVelha.py
+++ jogoDaVelha.py
@@ -1,9 +1,31 @@
-# tabuleiro = [
-# ['','',''],
-# ['','',''],
-# ['','','']
-# ]
+tabuleiro = [
+['','',''],
+['','',''],
+['','','']
+]
 
-tabuleiro=[['' for _ in range(3)] for _ in range(3)]
+jogador='X'
+def exibeTabuleiro():
+    for linha in tabuleiro:
+        """Mesma coisa imprimir de outra forma"""
+        # print(f'{linha[0]}|{linha[1]}|{linha[2]}')
+        # print('-----')
+        print('|'.join(linha))
+        print('-'*5)
 
-print(tabuleiro)
+def jogada(linha,coluna):
+    tabuleiro[linha][coluna]=jogador
+    if jogador =='X':
+        return 'O'
+    else:
+        return 'X'
+
+jogador=jogada(1,1)
+jogador=jogada(1,2)
+
+exibeTabuleiro()
+
+
+"""Uma outra forma de fazer utilizando o for"""
+# tabuleiro=[['' for _ in range(3)] for _ in range(3)]
+# print(tabuleiro)
