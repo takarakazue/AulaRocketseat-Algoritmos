31a17207bd17e2ca7474847b0a02559983b554d2
takara kazue <eugenia.stefens.237219@a.fecaf.com.br>
2026-03-24 18:35:29 +0900

    Aula Jogo da Velha Parte 2

--- jogoDaVelha.py
+++ jogoDaVelha.py
@@ -14,17 +14,48 @@ def exibeTabuleiro():
         print('-'*5)
 
 def jogada(linha,coluna):
+    if (
+        not 0 <= linha <= 2 or 
+        not 0 <= coluna <= 2 or 
+        tabuleiro [linha][coluna] != ''
+    ):
+        print('Jogada invalida!')
+        return  jogador
+
+# def jogada(linha,coluna):
+#     if not 0 <= linha <= 2:
+#         print('Jogada invalida!')
+#         return  jogador
+#     if not 0 <= coluna <= 2:
+#         print('Jogada invalida!')
+#         return  jogador
+#     if tabuleiro [linha][coluna] != '':
+#         print('Jogada invalida!')
+#         return  jogador
+    
     tabuleiro[linha][coluna]=jogador
-    if jogador =='X':
-        return 'O'
-    else:
-        return 'X'
+    # if jogador =='X':
+    #     return 'O'
+    # else:
+    #     return 'X'
+    return 'O' if jogador == 'X' else 'X'
 
-jogador=jogada(1,1)
-jogador=jogada(1,2)
+# jogador=jogada(1,1)
+# jogador=jogada(1,2)
+# jogador=jogada(2,2)
+# exibeTabuleiro()
 
-exibeTabuleiro()
-
+while True:
+    print(f'Jogador da vez: {jogador}')
+    try:
+        linha=int(input('Digite a linha: '))
+        coluna=int(input('Digite a coluna: '))
+        jogador=jogada(linha, coluna)
+    except IndexError:
+        print('Digite valores numericos entre 0 e 2')
+    except ValueError:
+        print('Os valores devem ser numeros inteiros')
+    exibeTabuleiro()
 
 """Uma outra forma de fazer utilizando o for"""
 # tabuleiro=[['' for _ in range(3)] for _ in range(3)]
