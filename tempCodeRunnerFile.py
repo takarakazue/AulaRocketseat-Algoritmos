54e04536c73110980a7ef9f57a2f7db6608eea76
takara kazue <eugenia.stefens.237219@a.fecaf.com.br>
2026-03-24 23:45:47 +0900

    ultima aula de debug

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,8 @@
-for index in range (1,11):
-#        print(index)
-#        soma+=index
+comando = "clima"
+match comando:
+    case "oi" | "olá":
+        print("Oi, como vai você?")
+    case "clima" | "previsão do tempo":
+        print("Está muito quente. Deve ter passado de 40 graus.")
+    case _:
+        print("Desculpe, não entendi o comando.")
---
+++ teste.py
@@ -0,0 +1,5 @@
+soma = 0
+for num in range(1, 5):
+    soma += num
+
+print(soma)
